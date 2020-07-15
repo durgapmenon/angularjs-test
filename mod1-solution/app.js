c15204fcde2ec388c04359bd70f3e37af1bc4708
Durga <durgapmenon@gmail.com>
2020-07-15 16:08:10 +0530

    Message

--- mod1-solution/app.js
+++ mod1-solution/app.js
@@ -1,5 +1,20 @@
 (function(){
   'use strict';
   angular.module('LunchCheck',[])
-  .controller('LunchCheckController',function($scope){});
+  .controller('LunchCheckController',function($scope){
+    $scope.checker = function(){
+      if ($scope.lunchList.length == 0){
+        $scope.output= "Please enter data first";
+      }else{
+        var lunchArr = $scope.lunchList.split(',');
+        if (lunchArr.length>3)
+        {
+          $scope.output="Too Much!";
+        }
+        else{
+          $scope.output="Enjoy!";
+        }
+      }
+    }
+  });
 })();

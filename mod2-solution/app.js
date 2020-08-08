c20424d12bf3adf059f233c15771cd61e9bbab32
Durga <durgapmenon@gmail.com>
2020-08-08 13:42:58 +0530

    Message

--- mod2-solution/app.js
+++ mod2-solution/app.js
@@ -1,50 +1,53 @@
 (function(){
   'use strict';
-
   angular.module('ShoppingListCheckOff',[])
   .controller('ToBuyController',ToBuyController)
   .controller('AlreadyBoughtController',AlreadyBoughtController)
   .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
-  var toBuy = [
-    {
-      name: "Milk",
-      quantity: "2"
-    },
-    {
-      name: "Donuts",
-      quantity: "200"
-    },
-    {
-      name: "Cookies",
-      quantity: "300"
-    },
-    {
-      name: "Chocolate",
-      quantity: "5"
-    }
-  ];
+
   ToBuyController.$inject=['ShoppingListCheckOffService'];
   function ToBuyController(ShoppingListCheckOffService){
     var notBought = this;
-    var items = ShoppingListCheckOffService.getToBuyItems();
-    var notBought.items = items;
-    var notBought.empty = (notBought.items.length == 0)? true:false;
-    var notBought.display = "Everything is bought.";
-    var notBought.markAsBought = function(itemIndex){
+    notBought.items = ShoppingListCheckOffService.getToBuyItems();
+    notBought.isEmpty = ShoppingListCheckOffService.isBuyEmpty;
+    notBought.display = "Everything is bought.";
+    notBought.markAsBought = function(itemIndex){
       ShoppingListCheckOffService.markAsBought(itemIndex);
-    }
+    };
   }
 
-  AlreadyBoughtController.$inject=['AlreadyBoughtController'];
+  AlreadyBoughtController.$inject=['ShoppingListCheckOffService'];
   function AlreadyBoughtController(ShoppingListCheckOffService){
     var alreadyBought = this;
-    var alreadyBought.items = ShoppingListCheckOffService.getAlreadyBoughtItems();
-    var alreadyBought.empty = (alreadyBought.items.length == 0)? true:false;
-    var alreadyBought.display = "Nothing Bought Yet";
+    alreadyBought.items = ShoppingListCheckOffService.getAlreadyBoughtItems();
+    alreadyBought.isEmpty = ShoppingListCheckOffService.isBoughtEmpty;
+    alreadyBought.display = "Nothing Bought Yet";
   }
 
   function ShoppingListCheckOffService(){
     var service = this;
+    var toBuy = [
+      {
+        name: "Milk Packets",
+        quantity: "2"
+      },
+      {
+        name: "Donuts",
+        quantity: "200"
+      },
+      {
+        name: "Cookies",
+        quantity: "300"
+      },
+      {
+        name: "Chocolate",
+        quantity: "5"
+      },
+      {
+        name: "Whisk",
+        quantity: "1"
+      },
+    ];
     var bought = [];
     service.getToBuyItems = function(){
       return toBuy;
@@ -55,6 +58,12 @@
     service.markAsBought = function(itemIndex){
         bought.push(toBuy[itemIndex]);
         toBuy.splice(itemIndex,1);
+    };
+    service.isBuyEmpty = function(){
+      return (toBuy.length === 0)? true:false;
+    };
+    service.isBoughtEmpty = function(){
+      return (bought.length === 0)? true:false;
     };
   }
 })();
